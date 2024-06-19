de32a70fdf0800c9bf1b9748a614196ffac7cb9e
shanelmanimtim <21-12973@g.batstate-u.edu.ph>
2024-06-19 11:27:43 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('APPchara'),
+        ),
+        backgroundColor: Colors.cyan[300],
+      ),
+      body: Center(
+        child: Image.network('https://www.hungryhuy.com/wp-content/uploads/atchara-pickle-jars.jpg')
+          ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: Colors.cyan[300],
+        child: Icon(
+            Icons.add,
+            color: Colors.black,
+        ),
+      ),
+    ),
+  ));
+}
