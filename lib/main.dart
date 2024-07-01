ccc515b8b78265f2e0e44e8e1e942e3c84c415e2
shanelmanimtim <21-12973@g.batstate-u.edu.ph>
2024-07-01 11:35:13 +0800

    Sign up page

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:untitled1/Home.dart';
+import 'package:untitled1/pages/Signup.dart';
 import 'package:untitled1/pages/dashboard.dart';
 import 'package:untitled1/pages/menu.dart';
 
 void main() => runApp(MaterialApp(
-    initialRoute: '/menu',
+    initialRoute: '/Signup',
     routes: {
       '/' : (context) => Dashboard(),
       '/menu' : (context) => Menu(),
       '/profile' : (context) => Home(),
+      '/Signup' : (context) => Signup(),
     },
   ));
 
---
+++ lib/pages/Signup.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+class Signup extends StatefulWidget {
+  const Signup({super.key});
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  final formKey = GlobalKey<FormState>();
+  String name = '';
+  String email = '';
+  String password = '';
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(15.0, 50.0, 10.0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Lets Get Started',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 2.0,
+                  fontSize: 24.5,
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    TextFormField(
+                      maxLength: 50,
+                      decoration: InputDecoration(
+                        label: Text('Name'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        )
+                      ),
+                      validator: (value){
+                        if(value == null || value.isEmpty){
+                          return 'LAGYAN NGG PANGALAN!';
+                        }
+                        if(value.length < 2){
+                          return 'DAPAT TATLONG LETRA PATAAS, boyop!';
+                        }
+                        return null;
+                      },
+                      onSaved: (value){
+                        name = value!;
+                      },
+                    ),
+                    SizedBox(height: 30.0,),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        label: Text('Email'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                      validator: (value){
+                        if(value == null || value.isEmpty){
+                          return 'MAGLAGAY KA DAW, BOBO!';
+                        }
+                      },
+                      onSaved: (value){
+                        email = value!;
+                      },
+                    ),
+                   SizedBox(height: 30.0,),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        label: Text('Password'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                      ),
+                      validator: (value){
+                        if(value == null || value.isEmpty){
+                          return 'LAGYAN MO BOBO KA!';
+                        }
+                        if(value.length < 8){
+                          return '8 Characters nga daw beh';
+                        }
+                        if(value.length > 20){
+                          return 'Abay bawase at napahaba ka naman!';
+                        }
+                        return null;
+                      },
+                      onSaved: (value){
+                        password = value!;
+                      },
+                    ),
+                    SizedBox(height: 25.0,),
+                    ElevatedButton(
+                        onPressed: (){
+                           if(formKey.currentState!.validate()){
+                             formKey.currentState!.save();
+                             print(name);
+                             print(email);
+                             print(password);
+                           }
+                        },
+                        child: Text('Sign Up'),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.pinkAccent,
+                          foregroundColor: Colors.white,
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
