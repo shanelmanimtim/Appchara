ba2da34dd1a8640f9a2ca7aa730a3e65c1a3da9e
shanelmanimtim <21-12973@g.batstate-u.edu.ph>
2024-07-08 15:26:48 +0800

    Activity 4

--- lib/pages/selectedProduct.dart
+++ lib/pages/selectedProduct.dart
@@ -6,11 +6,21 @@ class selectedProduct extends StatefulWidget {
   const selectedProduct({super.key,required this.product});
 
   @override
-  State<selectedProduct> createState() => _selectedProductState();
+
+  State<selectedProduct> createState() => _selectedProductState(product: product);
 }
 
 class _selectedProductState extends State<selectedProduct> {
+  final Product product;
+  late double totalAmount = product.price;
+  int numbersOfOrders =1;
+  _selectedProductState({required this.product});
+
   @override
+  void initState(){
+    super.initState();
+    totalAmount = product.price;
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -28,17 +38,47 @@ class _selectedProductState extends State<selectedProduct> {
           Text(widget.product.productName),
           Text(widget.product.description),
           Text(widget.product.price.toString()),
-
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(widget.product.price.toString()),
+              Text(
+                '₱ ${totalAmount.toString()}',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
             ],
           ),
-          SizedBox(height: 30.0,)
-          IconButton(
-              onPressed:
-              onPressed(){},
-              icon: ic)
+         Row(
+          children: [
+            IconButton(
+                onPressed: (){
+                  setState(() {
+                    if(numbersOfOrders > 1){
+                      numbersOfOrders -= 1;
+                      totalAmount = product.price * numbersOfOrders;
+                    }
+                  });
+                },
+                icon: Icon(Icons.remove),
+            ),
+            Text(
+              numbersOfOrders.toString(),
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+                IconButton(
+                onPressed:(){
+                  setState(() {
+                    numbersOfOrders += 1;
+                    totalAmount = product.price * numbersOfOrders;
+                  });
+                },
+                icon: Icon(Icons.add),
+            ),
+           ],
+         ),
         ],
       ),
 
